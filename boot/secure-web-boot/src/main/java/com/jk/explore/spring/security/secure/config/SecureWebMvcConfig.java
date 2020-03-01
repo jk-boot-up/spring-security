02c9487f57c0951ede1f4ecd18bca7c118af2762
jayasekhar <noreply@dummy.noreply.com>
2020-03-01 16:06:25 +0530

    sample web app with boot and security taken from spring boot samples

---
+++ boot/secure-web-boot/src/main/java/com/jk/explore/spring/security/secure/config/SecureWebMvcConfig.java
@@ -0,0 +1,17 @@
+package com.jk.explore.spring.security.secure.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class SecureWebMvcConfig implements WebMvcConfigurer {
+
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/home").setViewName("home");
+		registry.addViewController("/").setViewName("home");
+		registry.addViewController("/hello").setViewName("hello");
+		registry.addViewController("/login").setViewName("login");
+	}
+
+}
